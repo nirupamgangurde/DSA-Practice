677879938e40d90753101a5553d193973740eac1
nirupamgangurde <nirupamgangurde242@gmail.com>
2025-04-15 19:30:12 +0530

    added one more solution in this file problems_on_functional_recursion.py

--- problems_on_functional_recursion.py
+++ problems_on_functional_recursion.py
@@ -7,3 +7,14 @@ def rev(arr):
 arr = [1, 2, 3, 4, 5]
 rev_arr = rev(arr) 
 print(rev_arr)
+
+# check if stirng is palindrome or not using recursion
+def pal(s):
+    if len(s)<=1:
+        return True
+    if s[0] != s[-1]:
+        return False
+    return pal(s[1:-1])
+
+s = input("Enter a string: ")
+print(pal(s))
